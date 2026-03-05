6c23d8a7ed0038a421a9b502de6c30614e725406
Tanbir <pondit.tanbir@gmail.com>
2026-03-05 15:32:59 +0600

    add: Minor modification in welcome page

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -1,22 +1,28 @@
-import CustomerLayout from '@/Layouts/CustomerLayout';
-import { Head } from '@inertiajs/react';
-import HeroSection from '@/Components/Welcome/HeroSection';
-import FeaturedProducts from '@/Components/Welcome/FeaturedProducts';
-import BrandsGrid from '@/Components/Welcome/BrandsGrid';
-import CategoriesGrid from '@/Components/Welcome/CategoriesGrid';
-import ValueProposition from '@/Components/Welcome/ValueProposition';
-import CallToAction from '@/Components/Welcome/CallToAction';
+import CustomerLayout from "@/Layouts/CustomerLayout";
+import { Head } from "@inertiajs/react";
+import HeroSection from "@/Components/Welcome/HeroSection";
+import FeaturedProducts from "@/Components/Welcome/FeaturedProducts";
+import BrandsGrid from "@/Components/Welcome/BrandsGrid";
+import CategoriesGrid from "@/Components/Welcome/CategoriesGrid";
+import ValueProposition from "@/Components/Welcome/ValueProposition";
+import CallToAction from "@/Components/Welcome/CallToAction";
 
-export default function Welcome({ auth, brands, categories, featuredProducts = [], featuredProductsIsBestSelling = false }) {    
+export default function Welcome({
+    auth,
+    brands,
+    categories,
+    featuredProducts = [],
+    featuredProductsIsBestSelling = false,
+}) {
     return (
         <CustomerLayout auth={auth}>
             <Head title="Zam Zam Import export Inc - Landing Page" />
             <HeroSection />
+            <BrandsGrid brands={brands} />
             <FeaturedProducts
                 products={featuredProducts}
                 isBestSelling={featuredProductsIsBestSelling}
             />
-            <BrandsGrid brands={brands} />
             <CategoriesGrid categories={categories} />
             <ValueProposition />
             <CallToAction />
